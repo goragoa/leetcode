850b6d69e7eb3875cf53a4d2aff0c534f8eb1041
yantingting <1203424668@qq.com>
2020-04-20 19:26:24 +0800

    修改类名

--- ytt/QuickStartytt.java
+++ ytt/QuickStartytt.java
@@ -1,4 +1,5 @@
-public class QuickStart{
+public class QuickStartytt
+{
     public static void main(String[] args) {
         System.out.println("ytt");
     }
--- zhs/QuickStartzhs.java
+++ zhs/QuickStartzhs.java
@@ -1,4 +1,4 @@
-public class QuickStart{
+public class QuickStartzhs {
     public static void main(String[] args) {
         System.out.println("ss");
     }
