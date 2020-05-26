b4c73b52b314fd6f4e9b1290c4f0c67a9427d82a
zhangshuo <goragoa@outlook.com>
2020-05-26 14:15:27 +0800

    check in 2020/5/26

---
+++ zhs/_27zhs.java
@@ -0,0 +1,17 @@
+public class _27zhs {
+    public int removeElement(int[] nums, int val) {
+        int slowPointer = -1;
+        for (int fastPointer = 0; fastPointer < nums.length; fastPointer++) {
+            if (nums[fastPointer] != val) {
+                nums[++slowPointer] = nums[fastPointer];
+            }
+        }
+        return slowPointer + 1;
+    }
+
+    public static void main(String[] args) {
+        _27zhs test = new _27zhs();
+        int len = test.removeElement(new int[]{3,3,2,2},3);
+        System.out.println(len);
+    }
+}
