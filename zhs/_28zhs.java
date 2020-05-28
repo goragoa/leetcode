6b1b241719ef445d1aa348f2d0032bb419901755
zhangshuo <goragoa@outlook.com>
2020-05-28 15:59:30 +0800

    check in 2020/5/27

---
+++ zhs/_28zhs.java
@@ -0,0 +1,33 @@
+public class _28zhs {
+    public int charToInt(int idx, String s) {
+        return (int) s.charAt(idx) - (int) 'a';
+    }
+
+    public int strStr(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+        if (L > n) return -1;
+        int a = 26;
+        long modulus = (long) Math.pow(2, 31);
+        long h = 0, refH = 0;
+        for (int i = 0; i < L; i++) {
+            h = (h * a + charToInt(i, haystack)) % modulus;
+            refH = (refH * a + charToInt(i, needle)) % modulus;
+        }
+        if (h == refH) return 0;
+        long aL = 1;
+        for (int i = 1; i <= L; i++) {
+            aL = (aL * a) % modulus;
+        }
+        for (int i = 1; i < n - L + 1; i++) {
+            h = (h * a - charToInt(i - 1, haystack) * aL
+                    + charToInt(i + L - 1, haystack)) % modulus;
+            if (h == refH) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        _28zhs t = new _28zhs();
+        System.out.println(t.strStr("hello","ll"));
+    }
+}
---
+++ zhs/_31zhs.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class _31zhs {
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length <= 1) return;
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+        }
+        int start = i + 1, end = nums.length - 1;
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        _31zhs test = new _31zhs();
+        int[] t = new int[]{1, 1};
+        test.nextPermutation(t);
+        System.out.println(Arrays.toString(t));
+    }
+}
